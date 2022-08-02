b1d0ab6cfca552e0c3b7d29aad306d71a1eaa532
marlon307 <marlon-ramosb@hotmail.com>
2022-08-02 20:11:18 -0300

    First commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from 'react';
+import '../sass/globals.scss';
+import type { AppProps } from 'next/app';
+import Header from '../components/header/Header';
+import Footer from '../components/footer/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component { ...pageProps } />
+      <Footer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
